8614dd02c6077139b79f347fd9bbb9a595cdd976
MonaMouawad <mona.mouawad@dafagate.com>
2025-10-16 18:23:20 +0300

    add PatientName

--- lib/data/createDB.dart
+++ lib/data/createDB.dart
@@ -32,12 +32,15 @@ createDatabase() async
 
     await database
         .execute(
-            'CREATE TABLE report(id INTEGER PRIMARY KEY , date INTEGER ,analysisQuantity INTEGER ,AnalysisPrice REAL ,Labs_Id INTEGER ,'
+            'CREATE TABLE report(id INTEGER PRIMARY KEY , date INTEGER ,analysisQuantity INTEGER ,'
+                'AnalysisPrice REAL ,Labs_Id INTEGER ,'
                 'Representatives_Id INTEGER,AnalysisId INTEGER,'
             'FOREIGN KEY(Labs_Id) REFERENCES Labs (id),'
             'FOREIGN KEY(AnalysisId) REFERENCES Labs (AnalysisId),'
             'FOREIGN KEY(Representatives_Id) REFERENCES Representatives (id) )')
         .then((value) => log('create report db'));
+
+
 
 
     await database
@@ -46,14 +49,36 @@ createDatabase() async
                 'Representatives_Id INTEGER,'
             'FOREIGN KEY(Representatives_Id) REFERENCES Representatives (id) )')
         .then((value) => log('create RepresentativesDayCost db'));
-  }, onOpen: (database) async {
+  },
+      onOpen: (database) async {
+        try {
+          final result = await database.rawQuery(
+              "PRAGMA table_info(report)"
+          );
+
+          final columnExists = result.any(
+                  (col) => col['name'] == 'PatientName'
+          );
+
+          if (!columnExists) {
+            await database.execute(
+                'ALTER TABLE report ADD COLUMN PatientName TEXT'
+            );
+            log('PatientName column added successfully');
+          } else {
+            log('PatientName column already exists');
+          }
+        } catch (e) {
+          log('Error: $e');
+        }
 
   }).then((value) async => database = value
 
    );
 
 
- // // await Labs.DeleteLabs();
+
+  // // await Labs.DeleteLabs();
   if(AppConstants.appFirstOpen==false){
     await Labs.loadLabsCSV();
     await Representatives.insertRepresentatives();
